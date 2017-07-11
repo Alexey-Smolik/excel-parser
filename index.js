d16886fb66ee1d2969131e11b04d8557a4866ba8
Alexey <smolikvtanke@gmail.com>
2017-07-11 17:22:30 +0300

    excel

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 "use strict";
 const XLSX = require('xlsx');
-const pg = require('pg');
 const Sequelize = require("sequelize");
+const excelbuilder = require('msexcel-builder');
 
 const workbook = XLSX.readFile('test.xlsx');
 const list = workbook.Sheets["Лист1"];
 const sequelize = new Sequelize('postgres://postgres:11111@localhost:5432/mvd', {logging: false});
+const xlsxfile = excelbuilder.createWorkbook('./', 'error.xlsx');
+const sheet = xlsxfile.createSheet('Лист1', 13, 100);
 
-//const policemen = [];
 
-for (let row = 3; row <= 3; row++) {
-    sequelize.query('INSERT into policemans (id, full_name, post, email, work_phone, mobile_phone, location, work_territory, image_url, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
-        { bind: [parseInt(list['A' + row].h.substring(0, list['A' + row].h.length - 1)),
-            list['B' + row].h + ' ' + list['C' + row].h + ' ' + list['D' + row].h,
+
+for(let column = 1; column <= 13; column++){
+    sheet.set(column, 1, list[String.fromCharCode(64 + column) + 2].h);
+}
+
+var error_row = 2;
+for (let row = 3; row <= 4; row++) {
+    sequelize.query('INSERT into policemans (full_name, post, email, work_phone, mobile_phone, location, work_territory, image_url, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
+        { bind: [list['B' + row].h + ' ' + list['C' + row].h + ' ' + list['D' + row].h,
             list['G' + row].h,
             list['I' + row].h,
-            list['J' + row].h,
+            'jgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjgjg',
+            //list['J' + row].h,
             list['K' + row].h,
             list['H' + row].h,
             list['L' + row].h,
@@ -23,33 +30,25 @@ for (let row = 3; row <= 3; row++) {
             new Date(),
             new Date()]})
         .then(result => {
-            console.log(result[1]);
-        });
-
+            console.log("The line has been added successfully");
+        })
+        .catch(err => {
+            for(let error_col = 1; error_col <= 13; error_col++){
+                //sheet.set(error_col, error_row, 'test');
+                //console.log(list[String.fromCharCode(64 + error_col) + row].h);
+                sheet.set(error_col, error_row, list[String.fromCharCode(64 + error_col) + row].h);
+            }
+            error_row++;
+            console.error(err.message);
+        })
 }
 
 
-
-
-/*var obj = {
- id: parseInt(list['A' + row].h.substring(0, list['A' + row].h.length - 1)),
- full_name: list['B' + row].h + ' ' + list['C' + row].h + ' ' + list['D' + row].h,
- post: list['G' + row].h,
- email: list['I' + row].h,
- work_phone: list['J' + row].h,
- mobile_phone: list['K' + row].h,
- location: list['H' + row].h,
- work_territory: list['L' + row].h,
- image_url: list['M' + row].h
- };
- policemen.push(obj);*/
-
-
-/*sequelize.query('INSERT into policemans (id, full_name, post, email, work_phone, mobile_phone, location, work_territory, image_url, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
-    { bind: [policemen[0].id, policemen[0].full_name, policemen[0].post,
-        policemen[0].email, policemen[0].work_phone, policemen[0].mobile_phone,
-        policemen[0].location, policemen[0].work_territory, policemen[0].image_url,
-        new Date(), new Date()]})
-    .then(result => {
-        console.log();
-    });*/
+xlsxfile.save(function(result){
+    if (result == null)
+        console.log('error book created');
+    else{
+        console.error('error book was not created');
+        xlsxfile.cancel();
+    }
+});
